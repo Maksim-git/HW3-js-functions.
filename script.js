45363bd5a41c42af10d3f801edc6c4c53d0d6c23
Maksim-git <dv.maksim11@gmail.com>
2022-03-25 21:09:09 +0200

    completed 4th task

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('------------- # 4')
+function summation(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(summation(55, 5))
+
+let sum = function(subtr1, subtr2){
+    return subtr1 - subtr2;
+}
+
+console.log(sum(16, 5))
+
+function multiplication(multip1, multip2){
+    return multip1 * multip2;
+}
+
+console.log(multiplication(5,6))
+
+function division(division1, division2){
+    return division1 / division2;
+}
+
+console.log(division(10,2))
