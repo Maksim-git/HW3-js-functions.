2bccf51b1d80a9cf25e5f08d2ded7a1a952cdb20
Maksim-git <dv.maksim11@gmail.com>
2022-03-27 10:29:42 +0300

    tasks completed - 5,6,7,8,9

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('------------- # 4')
+
 function summation(num1, num2) {
     return num1 + num2;
 }
@@ -22,3 +23,52 @@ function division(division1, division2){
 }
 
 console.log(division(10,2))
+
+console.log('------------- # 5')
+
+
+function outputNumbers (number){
+    let numbers = '';
+    for (i = 1; i <= number; i++){
+        numbers = numbers + i + ' ';
+    }
+    return numbers
+}
+
+console.log(outputNumbers(5))
+
+console.log('------------- # 6')
+
+function countdown (downNumber){
+    let toStr = '';
+    for (i = downNumber; i >= 1; --i){
+        toStr = toStr + i + ' ';
+    }
+    return toStr
+}
+
+console.log(countdown(5))
+
+console.log('------------- # 7')
+
+function pow(x,n){
+    return x ** n
+}
+
+console.log(pow(2,2))
+
+console.log('------------- # 8')
+
+function isBigger(a, b){
+    return a > b;
+}
+
+console.log(isBigger(5, -1))
+
+console.log('------------- # 9')
+
+function isSmaller(c, d){
+    return c < d;
+}
+
+console.log(isSmaller(5, -1))
